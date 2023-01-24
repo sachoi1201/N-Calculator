b3d10753d983cb469b6199c1f9e9a7cdc792a63c
sachoi1201 <sachoi1201@naver.com>
2023-01-24 16:07:58 +0900

    initial - html

--- src/initial.js
+++ src/initial.js
@@ -10,9 +10,49 @@ export default class Initial {
     this.$initial = document.querySelector(".initial");
     this.$count = document.querySelector(".count");
     this.$person = document.querySelector(".person");
+    this.$names = document.querySelector(".initial__names");
   }
-  init() {}
+  init() {
+    // personSelector
+    for (const person of personArray) {
+      const option = document.createElement("option");
+      option.innerText = person;
+      this.$person.appendChild(option);
+    }
+    // countSelector
+    for (const count of countArray) {
+      const option = document.createElement("option");
+      option.innerText = count;
+      this.$count.appendChild(option);
+    }
+  }
+  event() {
+    // person
+    this.$person.addEventListener("input", () => {
+      this.personNumber = this.$person.value;
+      this.nameMake();
+    });
+    // count
+    this.$count.addEventListener("input", function () {
+      this.count = this.value;
+    });
+  }
+
+  nameMake = () => {
+    this.$names.innerHTML = "";
+    console.log(this.personNumber);
+    for (let i = 1; i <= this.personNumber; i++) {
+      const $name = document.createElement("input");
+      $name.setAttribute("key", i);
+      this.$names.appendChild($name);
+    }
+  };
+
   _hide() {
     this.initial.classList.add("hide");
+  }
+  render() {
+    this.init();
+    this.event();
   }
 }
---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+import Initial from "./initial.js";
+
+const initial = new Initial();
+initial.render();
